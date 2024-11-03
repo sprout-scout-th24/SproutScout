33cc0e6a301050f69bc9d2d201bacaa527c85b42
Gradon Burns <gradyhburns@gmail.com>
2024-11-03 01:48:18 -0600

    Beautification of the plant details

--- sproutscout/lib/pages/plant_details.dart
+++ sproutscout/lib/pages/plant_details.dart
@@ -11,7 +11,8 @@ class PlantDetailPage extends StatelessWidget {
   final plantBox = Boxes.getPlants();
   final TextEditingController nameController; // Controller for editing name
 
-  PlantDetailPage({required this.plant, required this.index, required this.onDelete})
+  PlantDetailPage(
+      {required this.plant, required this.index, required this.onDelete})
       : nameController = TextEditingController(text: plant.name);
 
   @override
@@ -33,29 +34,81 @@ class PlantDetailPage extends StatelessWidget {
               decoration: InputDecoration(labelText: 'Plant Name'),
             ),
             SizedBox(height: 16),
-            Text('Last Watered: ${dateFormat.format(plant.lastWetTime)}', style: TextStyle(fontSize: 18)),
-            SizedBox(height: 16),
-            Text('Moisture Status: ${plant.isMoistureHigh ? "High" : "Low"}', style: TextStyle(fontSize: 18)),
-            ElevatedButton(
-              onPressed: () async {
-                // Update the plant name in the box
-                plant.name = nameController.text; // Update the plant's name
-                await plantBox.putAt(index, plant); // Save the updated plant
-                onDelete(); // Notify the parent about the change
-                Navigator.of(context).pop();
-              },
-              child: Text('Save Changes'),
+            Text(
+              'Last Watered on ${dateFormat.format(plant.lastWetTime)}',
+              style: TextStyle(fontSize: 18),
             ),
-            ElevatedButton(
-              onPressed: () async {
-                // Delete the plant from the box
-                await deletePlantAtIndex(index);
-                onDelete(); // Notify the parent about the deletion
-                Navigator.of(context).pop();
-              },
-              style: ElevatedButton.styleFrom(backgroundColor: Colors.red[50]),
-              child: Icon(Icons.delete, color: Colors.red),
-            )
+            SizedBox(height: 16),
+            Text(
+              'Soil Moisture: ${plant.isMoistureHigh ? "High" : "Low"}',
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment
+                  .start, // Align buttons to the start of the row
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    // Update the plant name in the box
+                    plant.name = nameController.text; // Update the plant's name
+                    await plantBox.putAt(
+                        index, plant); // Save the updated plant
+                    onDelete(); // Notify the parent about the change
+                    Navigator.of(context).pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green[50],
+                    foregroundColor: Colors.green,
+                  ),
+                  child: Text('Save Changes'),
+                ),
+                SizedBox(width: 8), // Space between the buttons
+                ElevatedButton(
+                  onPressed: () async {
+                    // Show a confirmation dialog
+                    final shouldDelete = await showDialog<bool>(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Confirm Delete'),
+                          content: Text(
+                              'Are you sure you want to delete this plant?'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .pop(false); // Return false when canceled
+                              },
+                              child: Text('Cancel',
+                              style: TextStyle(color: Colors.grey)),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .pop(true); // Return true to confirm delete
+                              },
+                              child: Text('Confirm',
+                                  style: TextStyle(color: Colors.red)),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+
+                    // If the user confirmed, delete the plant
+                    if (shouldDelete == true) {
+                      await deletePlantAtIndex(index); // Delete the plant
+                      onDelete(); // Notify the parent about the deletion
+                      Navigator.of(context).pop(); // Close the main screen
+                    }
+                  },
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.red[50]),
+                  child: Icon(Icons.delete, color: Colors.red),
+                ),
+              ],
+            ),
           ],
         ),
       ),
@@ -65,7 +118,8 @@ class PlantDetailPage extends StatelessWidget {
 
 Future deletePlantAtIndex(int index) async {
   var box = Boxes.getPlants();
-  if (index >= 0 && index < box.length) { // Check if the index is valid
+  if (index >= 0 && index < box.length) {
+    // Check if the index is valid
     await box.deleteAt(index); // Deletes the plant at the specified index
   }
 }
