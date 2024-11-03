2007a898b0f4152b54ba54eb8972f30e987a97bd
Gradon Burns <gradyhburns@gmail.com>
2024-11-02 20:45:33 -0500

    Added Delete Button to Plant Details Window

--- sproutscout/lib/pages/plant_details.dart
+++ sproutscout/lib/pages/plant_details.dart
@@ -1,11 +1,14 @@
 // pages/plant_detail_page.dart
 import 'package:flutter/material.dart';
 import '../models/plant.dart'; // Adjust the import according to your file structure
+import 'package:hive/hive.dart';
 
 class PlantDetailPage extends StatelessWidget {
   final Plant plant;
+  final Box<Plant> plantBox;
+  final int index;
 
-  PlantDetailPage({required this.plant});
+  PlantDetailPage({required this.plant, required this.plantBox, required this.index});
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +21,21 @@ class PlantDetailPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Plant Name: ${plant.name}', style: TextStyle(fontSize: 24)),
             SizedBox(height: 16),
-            Text('Last Watered: ${plant.lastWetTime}', style: TextStyle(fontSize: 18)),
+            Text('Last Watered: ${plant.lastWetTime}',
+                style: TextStyle(fontSize: 18)),
             SizedBox(height: 16),
-            Text('Moisture Status: ${plant.isMoistureHigh ? "High" : "Low"}', style: TextStyle(fontSize: 18)),
+            Text('Moisture Status: ${plant.isMoistureHigh ? "High" : "Low"}',
+                style: TextStyle(fontSize: 18)),
+            ElevatedButton(
+              onPressed: () {
+                // Delete the plant from the box
+                plantBox.deleteAt(index);
+                Navigator.of(context).pop();
+              },
+              child: Icon(Icons.delete),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+            )
           ],
         ),
       ),
