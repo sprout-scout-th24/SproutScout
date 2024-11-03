59eef1ebc942e05f571fd25e1bb3477e46d6c097
Zach Goodson <quantamstudios@gmail.com>
2024-11-03 04:45:56 -0600

    Added Ability to Create Custom Plant Type

--- sproutscout/lib/pages/home.dart
+++ sproutscout/lib/pages/home.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:sproutscout/helpers/boxes.dart';
 import 'package:sproutscout/models/plant.dart';
+import 'package:sproutscout/models/plant_type.dart';
 import 'package:sproutscout/pages/plant_details.dart';
 
 class HomePage extends StatefulWidget {
@@ -131,7 +132,8 @@ class HomePageState extends State<HomePage> {
     final plantNameController = TextEditingController();
 
     // Retrieve plant types from the box
-    final List<String> plantTypes = Boxes.getPlantTypes().values
+    List<String> plantTypes = Boxes.getPlantTypes()
+        .values
         .map((plantType) => plantType.name)
         .toList();
     int selectedPlantTypeIndex = 0; // Default to the first type
@@ -141,49 +143,75 @@ class HomePageState extends State<HomePage> {
       builder: (context) {
         return AlertDialog(
           title: const Text('Add Plant'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                controller: plantNameController,
-                decoration: const InputDecoration(
-                  labelText: 'Name your plant!',
-                  labelStyle: TextStyle(color: Colors.grey),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green),
+          content: StatefulBuilder(
+            builder: (context, setState) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: plantNameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Name your plant!',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      floatingLabelStyle: TextStyle(color: Colors.green),
+                    ),
+                    style: const TextStyle(color: Colors.black),
                   ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
+                  const SizedBox(height: 20),
+                  DropdownButtonFormField<int>(
+                    value: selectedPlantTypeIndex,
+                    items: List<DropdownMenuItem<int>>.generate(
+                      plantTypes.length + 1, // Adding +1 for custom option
+                      (index) {
+                        if (index < plantTypes.length) {
+                          return DropdownMenuItem<int>(
+                            value: index,
+                            child: Text(plantTypes[index]),
+                          );
+                        } else {
+                          return const DropdownMenuItem<int>(
+                            value: -1, // Custom option value
+                            child: Text('Add Custom Plant Type'),
+                          );
+                        }
+                      },
+                    ),
+                    onChanged: (newIndex) {
+                      if (newIndex == -1) {
+                        // Show dialog for adding custom plant type
+                        _showAddCustomPlantTypeDialog(context,
+                            (customPlantType) {
+                          setState(() {
+                            // Update the plant types list and selected index
+                            plantTypes.add(customPlantType);
+                            selectedPlantTypeIndex = plantTypes.length -
+                                1; // Select the new plant type
+                          });
+                        });
+                      } else {
+                        selectedPlantTypeIndex = newIndex!;
+                      }
+                    },
+                    decoration: const InputDecoration(
+                      labelText: 'Select Plant Type',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green),
+                      ),
+                    ),
                   ),
-                  floatingLabelStyle: TextStyle(color: Colors.green),
-                ),
-                style: const TextStyle(color: Colors.black),
-              ),
-              const SizedBox(height: 20),
-              DropdownButtonFormField<int>(
-                value: selectedPlantTypeIndex,
-                items: List<DropdownMenuItem<int>>.generate(
-                  plantTypes.length,
-                  (index) => DropdownMenuItem<int>(
-                    value: index,
-                    child: Text(plantTypes[index]),
-                  ),
-                ),
-                onChanged: (newIndex) {
-                  selectedPlantTypeIndex = newIndex!;
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Select Plant Type',
-                  labelStyle: TextStyle(color: Colors.grey),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.green),
-                  ),
-                ),
-              ),
-            ],
+                ],
+              );
+            },
           ),
           actions: [
             TextButton(
@@ -204,6 +232,85 @@ class HomePageState extends State<HomePage> {
                 if (plantName.isNotEmpty) {
                   _addPlant(
                       plantName, selectedPlantTypeIndex); // Pass name and index
+                  Navigator.of(context).pop(); // Close the dialog
+                }
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showAddCustomPlantTypeDialog(
+      BuildContext context, Function(String) onPlantTypeAdded) {
+    final nameController = TextEditingController();
+    final frequencyController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Add Custom Plant Type'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Enter custom plant type',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                controller: frequencyController,
+                decoration: const InputDecoration(
+                  labelText: 'Enter the watering frequency',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.red[600],
+              ),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.green[600],
+              ),
+              onPressed: () {
+                final customPlantType = nameController.text;
+                if (customPlantType.isNotEmpty) {
+                  // Add the custom plant type to the list
+                  Boxes.getPlantTypes().add(PlantType(
+                    name: customPlantType,
+                    wateringFrequencySeconds:
+                        double.parse(frequencyController.text),
+                  ));
+
+                  // Call the callback function to update the main dialog
+                  onPlantTypeAdded(customPlantType);
                   Navigator.of(context).pop(); // Close the dialog
                 }
               },
